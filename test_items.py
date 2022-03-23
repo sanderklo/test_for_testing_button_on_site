2b84628acc01e889b4a0bcb6498f51aa78d7de36
vpsanderklo <sanderkloj@gmail.com>
2022-03-23 17:59:36 +0200

    Updating asert

--- test_items.py
+++ test_items.py
@@ -3,4 +3,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_for_adding_product_to_cart(browser):
     browser.get(link)
     button = browser.find_elements_by_css_selector("button.btn-add-to-basket")
-    assert button.is_displayed(), "Button add to cart, not displayed on web site"
+    assert button != [], "Button add to cart, not displayed on web site"
